2d11bffe1bce84fb2f3143bdecb9d46fd70a48a5
caterinadp <caterina.depaolis89@gmail.com>
2024-02-28 18:32:28 +0100

    added time functionality

--- javascript/index.js
+++ javascript/index.js
@@ -12,10 +12,32 @@ function updateTime() {
   let miamiElement = document.querySelector("#miami");
   let miamiDateElement = miamiElement.querySelector(".date");
   let miamiTimeElement = miamiElement.querySelector(".time");
-  let miamiTime = moment().tz("America/Miami");
+  let miamiTime = moment().tz("America/New_York");
 
   miamiDateElement.innerHTML = miamiTime.format("MMMM Do YYYY");
   miamiTimeElement.innerHTML = miamiTime.format("h:mm:ss [<small>]A[</small>]");
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
 updateTime();
-setInterval(updateTime, 1);
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
